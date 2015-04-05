6009c4f0ee4ffd7cd7a6e25192091761d6820ffe
TC Enders <tcenders1012@gmail.com>
2015-04-05 02:06:59 -0600

    JS Folder: Updated the index page

---
+++ scripts/twit.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+	$.ajax("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2")
+		.done(function(data) {
+			alert("Success!");
+			console.log(data);
+		})
+		.fail(function(data){
+			alert("Failure!");
+			console.log(data);
+		});
+});
