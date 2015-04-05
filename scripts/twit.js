46825c71e80ff60411271459362126abbf6864ab
TC Enders <tcenders1012@gmail.com>
2015-04-05 03:02:39 -0600

    Updated twit.js with a simple Flickr API call

--- scripts/twit.js
+++ scripts/twit.js
@@ -1,9 +1,9 @@
 /* WARNING!!!
  * Ty: Haven't tested this code yet
- *
+ * Flickr API Key: a6df65ab22ae2539575ab0427c4393d8
  */
 $(document).ready(function (){
-	$.ajax("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2")
+	$.ajax("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7f0c439ac459799265eb7fbbfd6c8117&tags=Fire&format=json&nojsoncallback=1&api_sig=469542d7601f559b0a1007a0e4283bda")
 		.done(function(data) {
 			alert("Success!");
 			console.log(data);
