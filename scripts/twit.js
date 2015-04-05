3d9ff94fd74feee014d1f8e913497d2c54299f9c
TC Enders <tcenders1012@gmail.com>
2015-04-05 02:13:23 -0600

    WARNING: twit.js has not been tested

--- scripts/twit.js
+++ scripts/twit.js
@@ -1,3 +1,7 @@
+/* WARNING!!!
+ * Ty: Haven't tested this code yet
+ *
+ */
 $(document).ready(function (){
 	$.ajax("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2")
 		.done(function(data) {
