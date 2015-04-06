394a76e61e789773bc6a564c7f0b71fe7a13461d
RaVenHelm <tybEnders@outlook.com>
2015-04-05 20:26:25 -0600

    Remove FB script

--- scripts/twit.js
+++ scripts/twit.js
@@ -5,17 +5,15 @@
 $(document).ready(function (){
 	$.ajax("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a6df65ab22ae2539575ab0427c4393d8&tags=Fire&format=json&nojsoncallback=1")
 		.done(function(data) {
-			alert("Success!");
-			//console.log(data);
+			console.log("Success!");
 			//$("#photoResult").html(data.photos.photo[1].title);
 			
-			//console.log(titles);
 			for(var i = 0; i < data.photos.photo.length; i++){
 				$("#titles").append("<li>" + data.photos.photo[i].title + "</li>");
 			} 
 		})
 		.fail(function(data){
-			alert("Failure!");
+			console.log("Failure!");
 			console.log(data);
 		});
 		
