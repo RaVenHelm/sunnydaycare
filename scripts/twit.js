b105682d16f08fe6747c3eb9bc18fdf31fc2ff5d
RaVenHelm <tybEnders@outlook.com>
2015-04-05 19:18:59 -0600

    Updated API Key

--- scripts/twit.js
+++ scripts/twit.js
@@ -1,13 +1,18 @@
-/* UNSTABLE: Hasn't been properly tested yet
+/* 
  * Simple use of an AJAX JQuery Call
  * Flickr API Key: a6df65ab22ae2539575ab0427c4393d8
  */
 $(document).ready(function (){
-	$.ajax("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7f0c439ac459799265eb7fbbfd6c8117&tags=Fire&format=json&nojsoncallback=1&api_sig=469542d7601f559b0a1007a0e4283bda")
+	$.ajax("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a6df65ab22ae2539575ab0427c4393d8&tags=Fire&format=json&nojsoncallback=1")
 		.done(function(data) {
 			alert("Success!");
-			console.log(data);
-			$("#photoResult").html(data.photos.photo[1].title);
+			//console.log(data);
+			//$("#photoResult").html(data.photos.photo[1].title);
+			
+			//console.log(titles);
+			for(var i = 0; i < data.photos.photo.length; i++){
+				$("#titles").append("<li>" + data.photos.photo[i].title + "</li>");
+			} 
 		})
 		.fail(function(data){
 			alert("Failure!");
@@ -16,3 +21,13 @@ $(document).ready(function (){
 		
 	
 });
+
+
+/* function getTitles(photos){
+	var result = [];
+	for (var i = 0; i < photos.length; i++){
+		result[i] = photos[i];
+	}
+	
+	return result;
+} */
