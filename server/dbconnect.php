8ebc91306bb1f412cbd92f5b1fdbf1199a863bfb
Tyberius Enders <tcenders1012@gmail.com>
2015-04-07 02:42:27 +0000

    Set up server/public folders

---
+++ server/dbconnect.php
@@ -0,0 +1,8 @@
+<?php 
+    /*
+     * This folder would include all the server-side scripts
+     * Here would be a simple PHP page
+     */
+     
+     echo "<h1>Hello World</h1>";
+?>
--- temp/Temp.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'TCEnders'
-
-def main():
-    print("Git is still working!")
-
-if __name__ == '__main__':
-    main()
