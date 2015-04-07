66c92fe9ededfd91a714fe85808f54c9e9f4f2d1
RaVenHelm <tybEnders@outlook.com>
2015-04-07 01:11:23 -0600

    PHP service error checking

--- temp/service.php
+++ temp/service.php
@@ -1,19 +1,28 @@
 <?php include("Temp.php");?>
 <?php 
     //Simple Web Service with PHP
+	//TODO: Maybe make the client id into a Regex Pattern?
     if(!isset($_GET["client_id"])){
         echo "Client id is not set";
     } else {
 		$result = "{ClientId:" . $_GET["client_id"];
 		$clientName = '';
+		$clientInt = 0;
 		
 		//Split the client id into two parts
 		$idSplit = explode("_", $_GET["client_id"]);
+		
 		//echo strlen($idSplit[1]);
 		
 		//Get the client id number (returns 0 if inval fails)
+		//If the delimiter is not "_" then idSplit would be an array with the GET value
 		//$clientInt = intval(substr($_GET["client_id"], -4));
-		$clientInt = intval($idSplit[1]);
+		if(count($idSplit) == 2){
+			$clientInt = intval($idSplit[1]);
+		} else {
+			echo 'Not a valid client ID';
+		    return; 
+		}
 		
 		//Make sure the last 4 digits are numeric
 		//Only 4 digits allowed for num
