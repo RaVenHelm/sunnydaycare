05886e94fff726502c73cb5c3d2cfb73fc8a6682
RaVenHelm <tybEnders@outlook.com>
2015-04-07 01:50:30 -0600

    Searchable Flickr Added

--- scripts/twit.js
+++ scripts/twit.js
@@ -3,18 +3,24 @@
  * Flickr API Key: a6df65ab22ae2539575ab0427c4393d8
  */
 $(document).ready(function (){
-	$.ajax("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a6df65ab22ae2539575ab0427c4393d8&tags=Fire&format=json&nojsoncallback=1")
-		.done(function(data) {
-			console.log("Success!");
-			//$("#photoResult").html(data.photos.photo[1].title);
-			
-			for(var i = 0; i < data.photos.photo.length; i++){
-				$("#titles").append("<li>" + data.photos.photo[i].title + "</li>");
-			} 
-		})
-		.fail(function(data){
-			console.log("Failure!");
-			console.log(data);
-			$("#photoResult").html("Could not connect to Flickr");
-		});
+	$("#photoSubmit").click(function(){
+		var urlString = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a6df65ab22ae2539575ab0427c4393d8&";
+		urlString += "&tags=" + $("#searchText").val();
+		urlString += "&format=json&nojsoncallback=1";
+		$.ajax(urlString)
+			.done(function(data) {
+				console.log("Success!");
+				console.log(data);
+				//$("#photoResult").html(data.photos.photo[1].title);
+				
+				for(var i = 0; i < data.photos.photo.length; i++){
+					$("#titles").append("<li>" + data.photos.photo[i].title + "</li>");
+				} 
+			})
+			.fail(function(data){
+				console.log("Failure!");
+				console.log(data);
+				$("#photoResult").html("Could not connect to Flickr");
+			});
+	});
 });
