7109605d062b60909b5ee77ae1db067c1ef3ed90
RaVenHelm <tybEnders@outlook.com>
2015-04-07 04:23:18 -0600

    PDO options for non-c9 servers

--- server/dbconnect.php
+++ server/dbconnect.php
@@ -18,18 +18,15 @@
      //Make sure DB is set-up before calling this method
      //If running from C9
      try{
-        $connection = new pdo('mysql:host=ravenhelm-sunnydaycare-1412043;dbname=sample_db',$username, $pass);
-     } catch (Exception $ex) {
+        //$connection = new pdo('mysql:host=ravenhelm-sunnydaycare-1412043;dbname=sample_db',$username, $pass);
+		$connection = new pdo('mysql:host=localhost;dbname=sample_db',$username);
+	 } catch (Exception $ex) {
          echo $ex->getMessage();
      }
      //Else
-     //$connection = new pdo('mysql:host=ravenhelm-sunnydaycare-1412043;dbname=sample_db',)
+     //$connection = new pdo('mysql:host=localhost;dbname=sample_db',$username, $pass);
+	 //If there is no password in the database users for YOUR server,
+	 //Do not add an argument for the password 
+    
      
-    //  $query = "SELECT * FROM users;";
-     
-    //  foreach($connection->query($query) as $row){
-    //      print_r($row);
-    //  }
-     
-     $connection = null;
 ?>
